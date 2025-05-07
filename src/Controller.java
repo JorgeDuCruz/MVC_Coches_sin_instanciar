c3fb30035803d3964495c5867f7f7557d735b346
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-05-07 13:47:40 +0200

    Arreglado fallo en el nombre de la funcion en el Controller

--- src/Controller.java
+++ src/Controller.java
@@ -36,7 +36,7 @@ public class Controller {
         return resultado;
     }
 
-    public static boolean mostrarVelocdiad(){
+    public static boolean mostrarVelocidad(){
         String matricula = View.pedirMatricula("Matricula del coche que quieres ver velocidad"); //TODO tratar el caso de matricula inexistente
         int velocidad = Model.getVelocidad(matricula);
         View.muestraVelocidad(matricula,velocidad);
