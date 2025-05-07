e82f50a502a96105e2ce9a3be883764b0806278c
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-05-07 14:32:37 +0200

    TODO Crear los tests del Controller

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         //punto de entrada a la aplicacion
         View.menu();
+
+        //TODO crear tests del controller y posiblemente modificar el controller para poder hacer los tests
     }
 }
