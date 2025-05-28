b5475f7c567dbd81eb268322e36a580e174911bb
Jorge Durán Cruz <jdurancruz@danielcastelao.org>
2025-05-28 13:49:34 +0200

    Empieza el examen

--- src/Coche.java
+++ src/Coche.java
@@ -2,10 +2,18 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
+    Integer gasolina;
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
+        this.gasolina=100;
+    }
+    public void setGasolina(int gasolina){
+        if (gasolina<0){
+            this.gasolina=0;
+        }
+        else {this.gasolina=gasolina;}
     }
 }
